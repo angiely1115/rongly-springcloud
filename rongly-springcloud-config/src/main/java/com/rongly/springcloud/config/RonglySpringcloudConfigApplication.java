878fdb90c706a800c46c06a15c5d3047a6a2772a
lvrongzhuan <lvrongzhuan@cnhnkj.com>
2018-11-07 17:28:50 +0800

    配置中心结合eureka实现高可用

--- rongly-springcloud-config/src/main/java/com/rongly/springcloud/config/RonglySpringcloudConfigApplication.java
+++ rongly-springcloud-config/src/main/java/com/rongly/springcloud/config/RonglySpringcloudConfigApplication.java
@@ -2,16 +2,20 @@ package com.rongly.springcloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-@EnableConfigServer
 /**
  * 当我config Server服务down 也不影响服务 因为在依赖服务启动的时候会pull一份文件到自己的临时目录
  */
+@SpringBootApplication
+@EnableConfigServer
+//让其他服务可以发现自己
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class RonglySpringcloudConfigApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RonglySpringcloudConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RonglySpringcloudConfigApplication.class, args);
+    }
 }
---
+++ rongly-springcloud-zuul/src/main/java/com/rongly/springcloud/zuul/config/okhttp/MyOkHttpClient.java
@@ -0,0 +1,88 @@
+package com.rongly.springcloud.feign.config.okhttp;
+
+
+import com.sun.org.apache.regexp.internal.RE;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @Author: lvrongzhuan
+ * @Description:
+ * @Date: 2018/10/31 15:25
+ * @Version: 1.0
+ * modified by:
+ */
+public class MyOkHttpClient {
+    private OkHttpClient okHttpClient;
+    private static final MediaType JSON_PARSE = MediaType.parse(org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+    public MyOkHttpClient(OkHttpClient okHttpClient) {
+        this.okHttpClient = okHttpClient;
+    }
+
+    public Response get(MyOkHttpBuilder myOkHttpBuilder){
+        Map<String,String> params = myOkHttpBuilder.getParams();
+        StringBuffer stringBuffer = new StringBuffer();
+        Optional.ofNullable(params).ifPresent((c->{
+            c.forEach((k,v)->{
+                if(stringBuffer.length()==0){
+                    stringBuffer.append(myOkHttpBuilder.getUrl());
+                    stringBuffer.append("?");
+                }else {
+                    stringBuffer.append("&");
+                }
+                stringBuffer.append(k).append("=").append(v);
+            });
+        }));
+
+        Request.Builder builder = this.createRequestBuilder(stringBuffer.toString(),myOkHttpBuilder.getHeadParams()).get();
+        Response response = null;
+        try {
+              response = okHttpClient.newCall(builder.build()).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    public Response post(MyOkHttpBuilder myOkHttpBuilder){
+        Request.Builder builder = this.createRequestBuilder(myOkHttpBuilder.getUrl(),myOkHttpBuilder.getHeadParams());
+        FormBody.Builder formBody = new FormBody.Builder();
+        Map<String,String> params = myOkHttpBuilder.getParams();
+        Optional.ofNullable(params).ifPresent(p->p.forEach(formBody::add));
+        builder.post(formBody.build());
+        Response response = null;
+        try {
+            response = okHttpClient.newCall(builder.build()).execute();
+          } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+
+
+    public Response postJsonBody(MyOkHttpBuilder myOkHttpBuilder){
+        Request.Builder builder = this.createRequestBuilder(myOkHttpBuilder.getUrl(),myOkHttpBuilder.getHeadParams());
+        RequestBody requestBody = RequestBody.create(JSON_PARSE,myOkHttpBuilder.getBody());
+        builder.post(requestBody);
+        Response response = null;
+        try {
+            response = okHttpClient.newCall(builder.build()).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+    private Request.Builder createRequestBuilder(String url,Map<String,String> headMaps){
+        Request.Builder builder = new Request.Builder().url(url);
+        Map<String,String> headParams = headMaps;
+        Optional.ofNullable(headParams).ifPresent(h->{
+            h.forEach(builder::addHeader);
+        });
+        return builder;
+    }
+}
