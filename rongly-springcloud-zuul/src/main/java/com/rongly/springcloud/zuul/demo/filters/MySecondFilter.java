b3edc9e5e151ca1279d160adf051573536168699
lvrongzhuan <lvrongzhuan@cnhnkj.com>
2018-12-26 18:16:06 +0800

    网关

--- rongly-springcloud-zuul/src/main/java/com/rongly/springcloud/zuul/demo/filters/MySecondFilter.java
+++ rongly-springcloud-zuul/src/main/java/com/rongly/springcloud/zuul/demo/filters/MySecondFilter.java
@@ -17,35 +17,35 @@ import javax.servlet.http.HttpServletRequest;
 public class MySecondFilter extends ZuulFilter {
     @Override
     public String filterType() {
-        return "pre";
+return "pre";
     }
 
     @Override
     public int filterOrder() {
-        return 0;
+return 0;
     }
 
     @Override
     public boolean shouldFilter() {
-        return true;
+return true;
     }
 
     @Override
     public Object run() throws ZuulException {
-        RequestContext requestContext = RequestContext.getCurrentContext();
-       HttpServletRequest httpServletRequest =  requestContext.getRequest();
-        String a = httpServletRequest.getParameter("a");
-        if(StringUtils.isBlank(a)){
-            //参数a没有传
-            requestContext.getResponse().setCharacterEncoding("UTF-8");
-            requestContext.setSendZuulResponse(false);//禁止下游服务的访问
-            requestContext.setResponseBody("{\"status\":\"403\",\"message\":                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              \"参数错误\"}");
-//            requestContext.setResponseStatusCode(403);
+RequestContext requestContext = RequestContext.getCurrentContext();
+HttpServletRequest httpServletRequest =  requestContext.getRequest();
+String a = httpServletRequest.getParameter("a");
+if(StringUtils.isBlank(a)){
+    //参数a没有传
+    requestContext.getResponse().setCharacterEncoding("UTF-8");
+    requestContext.setSendZuulResponse(false);//禁止下游服务的访问
+    requestContext.setResponseBody("{\"status\":\"403\",\"message\": \"参数错误\"}");
+//    requestContext.setResponseStatusCode(403);
 
-            requestContext.set("logic-is-success",false);
-            return null;
-        }
-        requestContext.set("logic-is-success",true);
-        return null;
+    requestContext.set("logic-is-success",false);
+    return null;
+}
+requestContext.set("logic-is-success",true);
+return null;
     }
 }
